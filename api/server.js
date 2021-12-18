97d9341d9d7186d876ccc971ba646f5c699a6b98
inayatmohammed <52902844+inayatmohammed@users.noreply.github.com>
2021-12-18 16:30:30 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+
 mongoose.connect('mongodb://127.0.0.1:27017/react-todo', {
 	useNewUrlParser: true, 
 	useUnifiedTopology: true 
